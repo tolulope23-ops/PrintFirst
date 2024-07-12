de39bca457988e6f8aa937ec3ab2c7ac3d0f7694
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2024-07-12 22:54:57 +0100

    SecondCss

--- printScript.js
+++ printScript.js
@@ -1,4 +1,4 @@
 window.addEventListener("scroll", function(){
     var navBar = this.document.querySelector("#navbar");
-    navBar.classList.toggle("sticky", this.window.scrollY > 0);
+    navBar.classList.toggle("sticky", this.window.scrollY < 0);
 })
